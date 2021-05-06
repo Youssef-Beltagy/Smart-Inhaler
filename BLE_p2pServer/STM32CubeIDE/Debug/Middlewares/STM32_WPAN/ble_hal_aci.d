c4ce8f60e8e539efdfdc096f081e0f2ec4b2f057
jenlee1919 <jenlee1919@live.com>
2021-05-06 09:35:57 -0700

    Template files for nucleo board
    Pins are initialized for switches, LEDs and FRAM SPI connection. The BLE still needs a bunch of work and contains dummy data and demo code right now.

---
+++ BLE_p2pServer/STM32CubeIDE/Debug/Middlewares/STM32_WPAN/ble_hal_aci.d
@@ -0,0 +1,23 @@
+Middlewares/STM32_WPAN/ble_hal_aci.o: \
+ C:/STM32Cube_FW_WB_V1.11.0/Middlewares/ST/STM32_WPAN/ble/core/auto/ble_hal_aci.c \
+ C:/STM32Cube_FW_WB_V1.11.0/Middlewares/ST/STM32_WPAN/ble/core/auto/ble_hal_aci.h \
+ C:/STM32Cube_FW_WB_V1.11.0/Middlewares/ST/STM32_WPAN/ble/core/auto/ble_types.h \
+ ../../../../../../../Middlewares/ST/STM32_WPAN/ble/core/template/compiler.h \
+ ../../../../../../../Middlewares/ST/STM32_WPAN/ble/core/template/ble_const.h \
+ ../../../../../../../Middlewares/ST/STM32_WPAN/ble/core/ble_std.h \
+ ../../../../../../../Middlewares/ST/STM32_WPAN/ble/core/ble_defs.h \
+ ../../../../../../../Middlewares/ST/STM32_WPAN/ble/core/template/osal.h
+
+C:/STM32Cube_FW_WB_V1.11.0/Middlewares/ST/STM32_WPAN/ble/core/auto/ble_hal_aci.h:
+
+C:/STM32Cube_FW_WB_V1.11.0/Middlewares/ST/STM32_WPAN/ble/core/auto/ble_types.h:
+
+../../../../../../../Middlewares/ST/STM32_WPAN/ble/core/template/compiler.h:
+
+../../../../../../../Middlewares/ST/STM32_WPAN/ble/core/template/ble_const.h:
+
+../../../../../../../Middlewares/ST/STM32_WPAN/ble/core/ble_std.h:
+
+../../../../../../../Middlewares/ST/STM32_WPAN/ble/core/ble_defs.h:
+
+../../../../../../../Middlewares/ST/STM32_WPAN/ble/core/template/osal.h:
