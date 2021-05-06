7a96d7fa51be3774b0b983b15777c0eceb4955b7
jenlee1919 <jenlee1919@live.com>
2021-05-05 20:37:14 -0700

    this is a sample
    not important for this one

---
+++ BLE_p2pServer/Debug/Middlewares/STM32_WPAN/osal.d
@@ -0,0 +1,5 @@
+Middlewares/STM32_WPAN/osal.o: \
+ C:/STM32Cube_FW_WB_V1.11.0/Middlewares/ST/STM32_WPAN/ble/core/template/osal.c \
+ C:/STM32Cube_FW_WB_V1.11.0/Middlewares/ST/STM32_WPAN/ble/core/template/osal.h
+
+C:/STM32Cube_FW_WB_V1.11.0/Middlewares/ST/STM32_WPAN/ble/core/template/osal.h:
