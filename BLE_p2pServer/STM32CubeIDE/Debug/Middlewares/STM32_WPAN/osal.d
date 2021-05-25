ffd6c1dd67a8edd366eb29935670e8720c82240e
Youssef Beltagy <yousseffouadbeltagy@gmail.com>
2021-05-25 08:27:49 -0700

    Cleaned Build files

--- BLE_p2pServer/STM32CubeIDE/Debug/Middlewares/STM32_WPAN/osal.d
+++
@@ -1,5 +0,0 @@
-Middlewares/STM32_WPAN/osal.o: \
- C:/Users/jenle/OneDrive/Desktop/School/CAPSTONE/Inhaler\ SW/Breathe/BLE_p2pServer/Middlewares/ST/STM32_WPAN/ble/core/template/osal.c \
- C:/Users/jenle/OneDrive/Desktop/School/CAPSTONE/Inhaler\ SW/Breathe/BLE_p2pServer/Middlewares/ST/STM32_WPAN/ble/core/template/osal.h
-
-C:/Users/jenle/OneDrive/Desktop/School/CAPSTONE/Inhaler\ SW/Breathe/BLE_p2pServer/Middlewares/ST/STM32_WPAN/ble/core/template/osal.h:
