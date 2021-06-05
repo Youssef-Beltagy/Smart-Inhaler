d8552353bf05e2e60fcfbe0dc74035c1a4996149
jenlee1919 <jenlee1919@live.com>
2021-06-05 10:59:58 -0700

    Update from my file
    This adds the LED toggling based on PVD value to the SW2 button (app_entry.c)
    
    Removes wakeup timer on RTC (unused)
    
    Adds the PVD functionality
    
    Removes excess code from stm32wbxx_it.c
    
    Adds our LEDs to configuration file (stm32wbxx_nucleo.c and .h)
    
    Changes to low power advertising in app_ble.c file
    
    Removed excess timers and methods from p2p_server_app.c

--- BLE_p2pServer/Core/Src/app_entry.c
+++ BLE_p2pServer/Core/Src/app_entry.c
@@ -51,7 +51,6 @@ extern RTC_HandleTypeDef hrtc;
 
 /* Private macros ------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
-
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
@@ -65,6 +64,8 @@ RTC_TimeTypeDef currentTime;
 RTC_DateTypeDef currentDate;
 time_t timestamp;
 struct tm currTime;
+extern __IO uint32_t flag;
+
 /* USER CODE END PV */
 
 /* Private functions prototypes-----------------------------------------------*/
@@ -212,6 +213,9 @@ static void Led_Init( void )
   BSP_LED_Init(LED_BLUE);
   BSP_LED_Init(LED_GREEN);
   BSP_LED_Init(LED_RED);
+  BSP_LED_Init(LED_GREEN_BR);
+  BSP_LED_Init(LED_YELLOW_BR);
+  BSP_LED_Init(LED_RED_BR);
 
   BSP_LED_On(LED_GREEN);
 #endif
@@ -289,7 +293,15 @@ void HAL_GPIO_EXTI_Callback( uint16_t GPIO_Pin )
 
     case BUTTON_SW2_PIN:
     	APP_BLE_Key_Button2_Action();
-    	BSP_LED_Toggle(LED1);
+    	//BSP_LED_Toggle(LED1);
+    	//BSP_LED_Toggle(LED_GREEN_BR);
+    	if (flag == 2) {
+    		BSP_LED_Toggle(LED_GREEN_BR);
+    	} else if (flag == 1) {
+    		BSP_LED_Toggle(LED_YELLOW_BR);
+    	} else if (flag == 0) {
+    		BSP_LED_Toggle(LED_RED_BR);
+    	}
 
     	HAL_RTC_GetTime(&hrtc, &currentTime, RTC_FORMAT_BIN);
     	HAL_RTC_GetDate(&hrtc, &currentDate, RTC_FORMAT_BIN);
