d58f05af377fd0f2e62f66696dcdcba978f73f1a
jenlee1919 <jenlee1919@live.com>
2021-06-06 15:43:48 -0700

    LEDs were being initialized twice
    Removed excess code. LEDs are already initialized in app.entry.c file

--- BLE_p2pServer/STM32CubeIDE/Application/User/Core/main.c
+++ BLE_p2pServer/STM32CubeIDE/Application/User/Core/main.c
@@ -543,22 +543,11 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
 
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, LED3_BR_Pin|LED1_BR_Pin|LED2_BR_Pin, GPIO_PIN_RESET);
-
   /*Configure GPIO pins : BUTTON_SW1_Pin SW1_BR_Pin */
   GPIO_InitStruct.Pin = BUTTON_SW1_Pin|SW1_BR_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : LED_GREEN_Pin LED_RED_Pin LED3_BR_Pin LED1_BR_Pin
-                           LED2_BR_Pin LED_BLUE_Pin */
-  GPIO_InitStruct.Pin = LED3_BR_Pin|LED1_BR_Pin|LED2_BR_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /*Configure GPIO pin : BUTTON_SW2_Pin */
   GPIO_InitStruct.Pin = BUTTON_SW2_Pin;
